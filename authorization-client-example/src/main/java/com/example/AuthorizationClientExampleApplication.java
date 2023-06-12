9b519f64118a32c769df22cb27e95d1a3ee80a3d
天玺 <17683906991@163.com>
2023-06-12 17:36:23 +0800

    添加客户端示例

---
+++ authorization-client-example/src/main/java/com/example/AuthorizationClientExampleApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Security OAuth2 Client启动类
+ *
+ * @author vains
+ */
+@SpringBootApplication
+public class AuthorizationClientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationClientExampleApplication.class, args);
+    }
+
+}
---
+++ authorization-client-example/src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试接口
+ *
+ * @author vains
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test01")
+    @PreAuthorize("hasAuthority('SCOPE_message.read')")
+    public String test01() {
+        return "test01";
+    }
+
+    @GetMapping("/test02")
+    @PreAuthorize("hasAuthority('SCOPE_message.write')")
+    public String test02() {
+        return "test02";
+    }
+
+    @GetMapping("/app")
+    @PreAuthorize("hasAuthority('app')")
+    public String app() {
+        return "app";
+    }
+
+}
