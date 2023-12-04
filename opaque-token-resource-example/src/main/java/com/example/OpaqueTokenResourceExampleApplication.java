ef78b9b099d9bd2fbd368f781e1f8636c03c4188
vains-Sofia <17683906991@163.com>
2023-12-04 11:16:22 +0800

    添加使用匿名token的资源服务示例

---
+++ opaque-token-resource-example/src/main/java/com/example/OpaqueTokenResourceExampleApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 资源服务匿名token示例服务
+ *
+ * @author vains
+ */
+@SpringBootApplication
+public class OpaqueTokenResourceExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpaqueTokenResourceExampleApplication.class, args);
+    }
+
+}
