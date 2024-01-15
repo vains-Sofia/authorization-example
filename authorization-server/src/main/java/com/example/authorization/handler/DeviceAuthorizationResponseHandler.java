11fbcc45629c32101d7eaaa1fed9108a61d48cbb
vains-Sofia <17683906991@163.com>
2024-01-15 15:27:56 +0800

    添加日志，定位问题，优化设备码登录接口

--- authorization-server/src/main/java/com/example/authorization/handler/DeviceAuthorizationResponseHandler.java
+++ authorization-server/src/main/java/com/example/authorization/handler/DeviceAuthorizationResponseHandler.java
@@ -5,6 +5,7 @@ import com.example.util.JsonUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -17,6 +18,7 @@ import java.nio.charset.StandardCharsets;
  *
  * @author vains
  */
+@Slf4j
 @RequiredArgsConstructor
 public class DeviceAuthorizationResponseHandler implements AuthenticationSuccessHandler {
 
@@ -27,6 +29,7 @@ public class DeviceAuthorizationResponseHandler implements AuthenticationSuccess
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        log.info("设备码验证成功，响应JSON.");
         // 写回json数据
         Result<Object> result = Result.success(deviceActivatedUri);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
