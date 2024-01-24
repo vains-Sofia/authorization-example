90db0ab5921beee796d4f4b409fb89fe0e8235f6
vains-Sofia <17683906991@163.com>
2024-01-24 17:12:47 +0800

    客户端回调地址中添加本地地址

--- authorization-server/src/main/java/com/example/repository/RedisRegisteredClientRepository.java
+++ authorization-server/src/main/java/com/example/repository/RedisRegisteredClientRepository.java
@@ -228,8 +228,9 @@ public class RedisRegisteredClientRepository implements RegisteredClientReposito
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.authorizationGrantType(new AuthorizationGrantType(SecurityConstants.GRANT_TYPE_SMS_CODE))
+                .authorizationGrantType(new AuthorizationGrantType(SecurityConstants.GRANT_TYPE_SMS_CODE))
                 // 授权码模式回调地址，oauth2.1已改为精准匹配，不能只设置域名，并且屏蔽了localhost，本机使用127.0.0.1访问
+                .redirectUri("http://127.0.0.1:5173/OAuth2Redirect")
                 .redirectUri("http://k7fsqkhtbx.cdhttp.cn/OAuth2Redirect")
                 .redirectUri("http://127.0.0.1:8000/login/oauth2/code/messaging-client-oidc")
                 // 该客户端的授权范围，OPENID与PROFILE是IdToken的scope，获取授权时请求OPENID的scope时认证服务会返回IdToken
@@ -298,6 +299,7 @@ public class RedisRegisteredClientRepository implements RegisteredClientReposito
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 // 授权码模式回调地址，oauth2.1已改为精准匹配，不能只设置域名，并且屏蔽了localhost，本机使用127.0.0.1访问
+                .redirectUri("http://127.0.0.1:5173/PkceRedirect")
                 .redirectUri("http://k7fsqkhtbx.cdhttp.cn/PkceRedirect")
                 // 开启 PKCE 流程
                 .clientSettings(builder.requireProofKey(Boolean.TRUE).build())
