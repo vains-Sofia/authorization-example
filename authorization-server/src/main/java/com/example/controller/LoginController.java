165ab82bd4bbf1fc1379c7e089b4f0e3196d9bee
vains-Sofia <17683906991@163.com>
2024-10-10 10:33:15 +0800

    feat：添加前后端分离、不分离对应yaml文件，修改登录接口获取验证码，修复图片验证码不显示图片的问题

--- authorization-server/src/main/java/com/example/controller/LoginController.java
+++ authorization-server/src/main/java/com/example/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.ShearCaptcha;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.example.model.Result;
 import com.example.model.response.CaptchaResult;
@@ -52,6 +54,18 @@ public class LoginController {
         // 存入缓存中，5分钟后过期
         redisOperator.set((IMAGE_CAPTCHA_PREFIX_KEY + id), captcha, DEFAULT_TIMEOUT_SECONDS);
         return Result.success("获取验证码成功.", new CaptchaResult(String.valueOf(id), captcha, (null)));
+    }
+
+    @GetMapping("/getCaptcha1")
+    public Result<CaptchaResult> getCaptcha1() {
+        // 使用huTool-captcha生成图形验证码
+        // 定义图形验证码的长、宽、验证码字符数、干扰线宽度
+        ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(130, 34, 4, 2);
+        // 生成一个唯一id
+        long id = IdWorker.getId();
+        // 存入缓存中，5分钟后过期
+        redisOperator.set((IMAGE_CAPTCHA_PREFIX_KEY + id), captcha.getCode(), DEFAULT_TIMEOUT_SECONDS);
+        return Result.success("获取验证码成功.", new CaptchaResult(String.valueOf(id), captcha.getCode(), captcha.getImageBase64Data()));
     }
 
 }
